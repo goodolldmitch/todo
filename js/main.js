9aaa8bf5c6508d0988fa8861f770db7aea221ec1
goodoldmitch <mitch.mithcel@yandex.ru>
2020-08-11 22:15:59 +0300

    Local storage added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var listItems;
+
+function addToStorage(){
+    listItems = tdl.innerHTML;
+    localStorage.setItem('listItems' , listItems);
+}
+
+
 // показываем форму по клику
 show_form.onclick = function () { // по клику на кнопку show_form
     document.getElementById('form').style.display = 'block'; // показываем форму
@@ -5,47 +13,55 @@ show_form.onclick = function () { // по клику на кнопку show_form
 };
 
 // обработка формы
-form.onsubmit = function(){
+form.onsubmit = function () {
     event.preventDefault(); // блокируем отправку по умолчанию
-    
+
     var task = document.getElementById('task').value; // берем значение из поля
-    
+
     var taskLine = document.createElement('li'); // создаем элемент списка    
 
     var date = document.getElementById('date').value; // берем значение из поля
 
-    taskLine.innerHTML = '<input type="checkbox" class="check">' + task + '<span class="deadline">' + date + '</span> <span class="delete">&times;</span>' ; // вставляем в элемент списка значение из поля формы
+    taskLine.innerHTML = '<input type="checkbox" class="check">' + task + '<span class="deadline">' + date + '</span> <span class="delete">&times;</span>'; // вставляем в элемент списка значение из поля формы
 
-    if(task != ""){ // если поле формы было заполнено
+    if (task != "") { // если поле формы было заполнено
         tdl.append(taskLine); // вставляем элемент в список
         document.getElementById('form').style.display = 'none'; // убираем форму
+        
+        addToStorage(); // записываем в хранилище 
     }
-    else{ // если не заполнено, выводим предупреждение
+    else { // если не заполнено, выводим предупреждение
         alert('Введи задачу');
     }
-    
+
 };
 
 // работа с задачей
-tdl.onclick = function(){ // нажимаем на строку списка и распределяем события в зависимости от места, куда был клик
-    
+tdl.onclick = function () { // нажимаем на строку списка и распределяем события в зависимости от места, куда был клик
+
     var clickedInnerItem = event.target; // получаем элемент по которому сделан клик
 
     var clickedInnerItemClass = event.target.className; // получаем класс кликнутого элемента
 
     var clickedInnerItemParent = clickedInnerItem.parentNode; // определяем родительский элемент
-    
-    if (clickedInnerItemClass == 'delete'){ // если клик по классу delete        
+
+    if (clickedInnerItemClass == 'delete') { // если клик по классу delete        
 
         clickedInnerItemParent.remove(); // удаляем родителя
+
+        addToStorage(); // записываем в хранилище 
     }
-    else if(clickedInnerItemClass == 'check'){ // если клик по чекбоксу (проверить чекнут ли чекбокс)
+    else if (clickedInnerItemClass == 'check') { // если клик по чекбоксу (проверить чекнут ли чекбокс)
 
         clickedInnerItemParent.classList.add('done'); // добавляем класс о готовности
+
+        addToStorage(); // записываем в хранилище 
     }
-    else{
+    else {
         alert('edit'); // реализовать редактирование
     }
 };
 
-
+if (localStorage.getItem('listItems')){
+    tdl.innerHTML = localStorage.getItem('listItems');
+}
